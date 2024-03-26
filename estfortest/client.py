5c271b266eb56a4e787c3820027ac1eeecd77d63
Sxnchxs <dxzim@bk.ru>
2024-03-26 17:23:10 +0500

    Add: startActions, but without settings.

--- estfortest/client.py
+++ estfortest/client.py
@@ -1,4 +1,5 @@
 from web3 import Web3
+from web3.middleware import construct_sign_and_send_raw_middleware
 from eth_account import Account
 from typing import Optional
 import requests
@@ -22,6 +23,7 @@ class Client:
         self.rpc = rpc
         self.w3 = Web3(Web3.HTTPProvider(endpoint_uri=self.rpc))
         self.address = Web3.to_checksum_address(self.w3.eth.account.from_key(private_key=private_key).address)
+        self.account = self.w3.middleware_onion.add(construct_sign_and_send_raw_middleware(self.private_key))
 
 
     # Get playerID from wallet address
@@ -44,3 +46,50 @@ class Client:
             address=Web3.to_checksum_address(contract_address),
             abi=Client.default_abi
         ).functions.players(playerID).call())
+    
+    def start_action(self, contract_address: str, playerID: int):
+
+        _attire = {
+            "attire": {
+                "head": 0,
+                "neck": 0,
+                "body": 0,
+                "arms": 0,
+                "legs": 0,
+                "feet": 0,
+                "ring": 0,
+                "reserved1": 0
+            },
+            "actionId": 506,
+            "regenerateId": 0,
+            "choiceId": 0,
+            "rightHandEquipmentTokenId": 2561,
+            "leftHandEquipmentTokenId": 0,
+            "timespan": 86400,
+            "combatStyle": 0
+        }
+        _queuedActions = [_attire]
+        _queuedStatus = 2
+
+        a = self.w3.eth.contract(
+            address=Web3.to_checksum_address(contract_address),
+            abi=Client.default_abi
+        ).functions.startActions(playerID, _queuedActions, _queuedStatus).build_transaction({
+            'from': self.address,
+            'gas': int(self.w3.eth.get_block('latest')["gasUsed"]),
+            'gasPrice': int(self.w3.eth.gas_price * 1.3),
+            'nonce': self.w3.eth.get_transaction_count(self.address)
+        })
+        
+        print(a["gas"])
+        print(a["gasPrice"])
+        # print(a['data'])
+        
+        
+        
+        signed_tx = self.w3.eth.account.sign_transaction(a, private_key=self.private_key)
+        tx_hash = self.w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        if receipt:
+            print('Transaction successfull')
+            print(tx_hash.hex)
--- estfortest/main.py
+++ estfortest/main.py
@@ -4,9 +4,11 @@ from data.config import private_key, ftm_rpc
 client = Client(private_key=private_key, rpc=ftm_rpc)
 
 playerid = client.active_player(contract_address="0x058ec56aba13f7fee3ae9c9b91b3bb03bc336143")
-claimedrewards = client.daily_claimed_rewards(contract_address="0x058ec56aba13f7fee3ae9c9b91b3bb03bc336143",
-                                               playerID=playerid)
-player_info = client.players_info(contract_address="0x058ec56aba13f7fee3ae9c9b91b3bb03bc336143",
-                                               playerID=playerid)
+#claimedrewards = client.daily_claimed_rewards(contract_address="0x058ec56aba13f7fee3ae9c9b91b3bb03bc336143", playerID=playerid)
+# player_info = client.players_info(contract_address="0x058ec56aba13f7fee3ae9c9b91b3bb03bc336143",
+#                                                playerID=playerid)
+
 print(playerid)
-print(player_info)
+#print(player_info)
+client.start_action(contract_address="0x058ec56aba13f7fee3ae9c9b91b3bb03bc336143",
+                                               playerID=playerid)
