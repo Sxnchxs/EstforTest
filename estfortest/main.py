4066d113b9ccf86efdfcca265d97c368ab4d7c41
Sxnchxs <dxzim@bk.ru>
2024-03-26 19:09:08 +0500

    Remake: add tx and verify tx

--- estfortest/main.py
+++ estfortest/main.py
@@ -8,7 +8,7 @@ playerid = client.active_player(contract_address="0x058ec56aba13f7fee3ae9c9b91b3
 # player_info = client.players_info(contract_address="0x058ec56aba13f7fee3ae9c9b91b3bb03bc336143",
 #                                                playerID=playerid)
 
-print(playerid)
+#print(playerid)
 #print(player_info)
-client.start_action(contract_address="0x058ec56aba13f7fee3ae9c9b91b3bb03bc336143",
-                                               playerID=playerid)
+tx = client.start_action(contract_address="0x058ec56aba13f7fee3ae9c9b91b3bb03bc336143", playerID=playerid)
+verify = client.verify_tx(tx)
